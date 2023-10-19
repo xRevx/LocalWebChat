b490f4b4a9f92827d4db26f610f3f58f78a14630
ori <oriaviad2006@gmail.com>
2023-10-19 23:41:28 +0300

    styling is amazing

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,22 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Register = () => {
-    return ( <>Register</> );
+    return (
+        <Form>
+          <Row>
+            <Col xs="6">
+             <Stack gap={3}>
+                <h2>
+                    Register
+                </h2>
+                <Form.Control type="text" placeholder="Name"/>
+                <Form.Control type="text" placeholder="Password"/>
+                <Form.Control type="text" placeholder="Re enter Password"/>
+             </Stack>
+            </Col>
+          </Row> 
+        </Form>
+    );
 }
  
 export default Register;
