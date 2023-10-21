87d6f168733dd210d776077a7d137da8f00b2504
ori <oriaviad2006@gmail.com>
2023-10-21 15:28:05 +0300

    having problem with sendindg data

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,10 +3,10 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 const Register = () => {
 
-    const {registerInfo, updateRegisterInfo} = useContext(AuthContext)
+    const {registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading} = useContext(AuthContext)
 
     return (
-        <Form>
+        <Form onSubmit={registerUser}>
           <Row style={{
             height: "100vh",
             justifyContent: "center",
@@ -27,9 +27,13 @@ const Register = () => {
                  onChange={(e) => updateRegisterInfo({...registerInfo, password: e.target.value})}/>
                  
                 <Button variant="primary" type="submit">
-                    Register
+                    {isRegisterLoading ? "Creating Your account" : "Register"}
                 </Button>
-                <Alert variant="danger" ><p>An error occured</p></Alert>
+                {
+                  registerError?.error && (
+                 <Alert variant="danger" ><p>{registerError?.message}</p>
+                 </Alert>
+                )}
              </Stack>
             </Col>
           </Row> 
