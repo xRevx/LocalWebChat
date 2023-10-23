f814d7e54a26611c937959414b402f9417ded49c
ori <oriaviad2006@gmail.com>
2023-10-23 20:08:50 +0300

    something went wrone

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,54 @@
+import { createContext, useCallback, useState } from "react";
+import { baseUrl, postRequest } from "../utils/services";
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+    const [registerError, setRegisterError] = useState(null)
+    const [isRegisterLoading, setIsRegisterLoading] = useState(false)
+    const [registerInfo, setRegisterInfo] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const updateRegisterInfo = useCallback((info) => {
+        setRegisterInfo(info);
+    },[])
+
+    
+    const registerUser = useCallback(async(e) => {
+        e.preventDefault()
+
+        setIsRegisterLoading(true)
+        setRegisterError(null)
+
+        const response = await postRequest(`${baseUrl}/users/register`,
+         JSON.stringify(registerInfo))
+
+        setIsRegisterLoading(false)
+
+        if(response.error){
+            return setRegisterError(response)
+        }
+
+        localStorage.setItem("User", JSON.stringify(response))
+        setUser(response)
+    },[registerInfo])
+
+    return (
+         <AuthContext.Provider
+        value={{
+            user,
+            registerInfo,
+            updateRegisterInfo,
+            registerUser,
+            registerError,
+            isRegisterLoading,
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
---
+++ client/src/utils/services.js
@@ -0,0 +1,27 @@
+export const baseUrl = "https://local:5000/api"
+
+export const postRequest = async(url, body) =>{
+    const response = await fetch(url,{
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body
+    })
+    const data = await response.json()
+
+    if (!response.ok){
+        let message 
+        
+        if(data?.message){
+            message = data.message
+        }
+        else{
+            message = data
+        }
+
+        return {error: true, message}
+    }
+
+    return data
+}
