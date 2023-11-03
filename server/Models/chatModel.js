34ec2f7d3776534d4f73e13fa01b7541e0adf227
Rev <oriaviad1@gmail.com>
2023-11-03 18:33:10 +0200

    ep 9 problem

---
+++ server/Models/chatModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const chatSchema = new mongoose.Schema({
+    members: Array,
+
+}, {
+    timeseries: true,
+
+}
+)
+
+const chatModel = new mongoose.model("Chat", chatSchema)
+
+module.exports = chatModel;
